0665b4fca1c59e720ec3d2191e70fd71ee309099
nefzina <nefzi.amani@gmail.com>
2023-03-17 00:16:12 +0100

    update fullPostIt in js and style file

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
 
-// function to open a post it in full size
-// const posts = document.querySelector(".active");
-
-// posts.addEventListener("click", () => {
-//     const rightSection = document.querySelector(".fullPostIt");
-//     const leftSection = document.querySelector(".postItSection");
-//     if (window.innerWidth > 600) {
-//         rightSection.classList.add('visible');
-//         leftSection.classList.add('small');
-//     }
-//     else {}
-// })
-
-
 document.onclick = (event) => {
     const rightSection = document.querySelector(".fullPostIt");
     const leftSection = document.querySelector(".postItSection");
-    if (window.innerWidth > 600 && event.target.classList.contains("postIt")) {
+    if (window.innerWidth > 600 && (event.target.parentElement.classList.contains("postIt") || event.target.classList.contains("postIt"))) {
         rightSection.classList.add('visible');
         leftSection.classList.add('small');
     }
-    else {}
 }
